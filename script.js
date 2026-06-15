9fcdce253808eb57bc93ceef2925cbd506c210a3
chander singh <chandersingh9b.gkjainschoois@gmail.com>
2026-06-15 14:03:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-task = task[];
+let task = [];
 
+Counter = (function() {
+    var count = 0;
+    return function() {
+        count++;
+        return count;
+    };
+})();
+
+function addTask() {
+    var input = document.getElementById("inputString").value;
+    ID = Counter();
+    task.push(input);
+    displayTask(task);
+}   
